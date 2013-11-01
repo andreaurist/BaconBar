6a8b8080fb1c23000f5bc74103faf8cecabdf8d9
andreaurist <andreaurist@yahoo.com>
2013-11-01 17:21:31 -0400

    tweaks to homepage gallery and gallery page

--- js/denihan.baconbar.page.load.js
+++ js/denihan.baconbar.page.load.js
@@ -70,3 +70,62 @@ function modernizr() {
         });
     }
 }
+
+
+var clicked = false;
+$(".img-swap").click(function(){
+  if(!clicked)
+    $(this).addClass("clicked")
+  else
+    this.src.replace("-off","-on");
+});
+
+
+$(".img-swap").hover(function () {
+      this.src = this.src.replace("-off","-on");
+  }, function () {
+     if(!$(this).hasClass("clicked"))	 
+       this.src = this.src.replace("-on","-off");
+ });
+
+
+jQuery(function(){
+	$("#pig2").click(function() {
+		$("#pig3").attr("src","img/core/pig-3-off.png");
+		$("#pig1").attr("src","img/core/pig-1-off.png");
+		$("#pig1").removeClass("clicked");
+		$("#pig3").removeClass("clicked");
+		$("#hero-main").fadeOut(200, function() {
+			$("#hero-main").attr("src",$("#hero2").attr("href"));
+		}).fadeIn(200);
+		return false;
+	});
+	$("#pig1").click(function() {
+		$("#pig3").attr("src","img/core/pig-3-off.png");
+		$("#pig2").attr("src","img/core/pig-2-off.png");
+		$("#pig2").removeClass("clicked");
+		$("#pig3").removeClass("clicked");
+		$("#hero-main").fadeOut(200, function() {
+			$("#hero-main").attr("src",$("#hero1").attr("href"));
+		}).fadeIn(200);
+		return false;
+	});
+	$("#pig3").click(function() {
+		$("#pig1").attr("src","img/core/pig-1-off.png");
+		$("#pig2").attr("src","img/core/pig-2-off.png");
+		$("#pig2").removeClass("clicked");
+		$("#pig1").removeClass("clicked");
+		$("#hero-main").fadeOut(200, function() {
+			$("#hero-main").attr("src",$("#hero3").attr("href"));
+		}).fadeIn(200);
+		return false;
+	});
+});
+
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+        new Image().src = this;
+    });
+}
+
+
