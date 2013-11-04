b69ff07d145af640da0034644779eea1e22b35d6
andreaurist <andreaurist@yahoo.com>
2013-11-04 11:23:26 -0500

    gallery CSS / home page her gallery CSS/JS tweaks

--- js/denihan.baconbar.page.load.js
+++ js/denihan.baconbar.page.load.js
@@ -128,4 +128,15 @@ function preload(arrayOfImages) {
     });
 }
 
-
+$(document).ready(function(){
+	var d = new Date();    // defaults to the current time in the current timezone
+	if (d.getHours() >= 19 ) {
+    	//show nightime pig hero
+    	$("#hero-main").attr("src","img/hero-home-night.jpg");
+    	$("#pig2").attr("src","img/core/pig-2-on.png");
+	} else {
+    	//show daytime pig hero
+    	$("#hero-main").attr("src","img/hero-home-day.jpg");
+    	$("#pig1").attr("src","img/core/pig-1-on.png");
+	}
+});
